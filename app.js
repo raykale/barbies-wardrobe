10c989213444e35732e2dfec57d1c8c4bd1ca78f
rachail Pomerlee <rachailpomerlee@rachails-MacBook-Pro.local>
2023-11-07 22:28:09 -0500

    I added red bottom functionality and hover styling I also added chanel functionality

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ class Career {
         this.id = id;
     }
 }
-
+//array of objects below
 const careerDescriptions = [
     {
         name: 'lawyer',
@@ -57,7 +57,7 @@ for (let i = 10 ; i > 0; i--){
  careers.push(new Career(job.name, job.description, income, `${job.name}-${income}` ))
 }
 
-
+//give barbie radom career
 barbie.career = careers[randomization(careers.length)]
 
 class Clothing {
@@ -72,6 +72,8 @@ class Clothing {
 }
 
 const birkin = new Clothing('Birkin Bag', 'Hermes', 'purple', 'bag', 'lg', 15470 )
+const redBottoms = new Clothing('Red Bottoms', 'Christian', 'black', 'shoes', '6', 3000)
+const chanel = new Clothing('Chanel', 'Melanie', 'gold', 'perfume', '8oz', 235)
 
 
 
@@ -131,3 +133,34 @@ workButton.addEventListener('click', ()=>{
     barbie.render();
 })
 
+const rbButton = document.getElementById('red-bottoms')
+
+rbButton.addEventListener('click', () => {
+    if (barbie.wallet >= redBottoms.price){
+        barbie.wardrobe.push(redBottoms);
+        barbie.wallet -= redBottoms.price;
+        barbie.render();
+        // WE updated the wardrobe that belongs to barbie so the object was changed
+    // the object control the information that is visible to us on the screen
+    // I want to re-render the content so that i can see the updated information in the browser
+    } else {
+        alert('Stop trippin you know you aint got it like that');
+    }
+
+})
+
+const chanelButton = document.getElementById('chanel')
+
+chanelButton.addEventListener('click', () => {
+    if (barbie.wallet >= chanel.price){
+        barbie.wardrobe.push(chanel);
+        barbie.wallet -= chanel.price;
+        barbie.render();
+        // WE updated the wardrobe that belongs to barbie so the object was changed
+    // the object control the information that is visible to us on the screen
+    // I want to re-render the content so that i can see the updated information in the browser
+    } else {
+        alert('Stop trippin you know you aint got it like that');
+    }
+
+})
